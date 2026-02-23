9b70c2bcb757610413bec743a73b2246a34dac28
SakibSibly <sibly@duck.com>
2026-02-23 09:31:33 +0600

    Add "Outlet added for shared components"

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -3,13 +3,9 @@ import Footer from "../Footer/Footer"
 
 const HomePage = () => {
     return (
-        <div>
-            <Navbar />
-                <div className="flex flex-col items-center justify-center h-screen">
-                    <h1 className="text-6xl font-bold text-gray-800">Home Page</h1>
-                    <p className="text-2xl text-gray-600 mt-4">Welcome to the Home Page</p>
-                </div>
-            <Footer />
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-6xl font-bold text-gray-800">Home Page</h1>
+            <p className="text-2xl text-gray-600 mt-4">Welcome to the Home Page</p>
         </div>
     )
 }
